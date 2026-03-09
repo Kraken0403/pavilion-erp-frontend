cbd33055dd6532df07da7ca741a6b105a1e4e198
Yash Wadhvani <yashwadhvani123@gmail.com>
2026-03-09 21:58:30 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React, { useMemo, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Sidebar from './Sidebar'; // Adjust the path based on your structure
+import Sidebar from './Sidebar';
 import { Box } from '@mui/material';
 import { LayoutProvider } from '../context/LayoutContext';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
     const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -18,15 +18,15 @@ const Layout = ({ children }) => {
     );
 
     return (
-        <LayoutProvider value={contextValue} key={location.pathname}>
+        <LayoutProvider value={contextValue}>
             <Box sx={{ display: 'flex' }}>
                 <div className={`sidebar-container ${sidebarOpen ? '' : 'collapsed'}`}>
                     <Sidebar />
                 </div>
 
                 <div
-                    className={`main-container ${sidebarOpen ? '' : 'expanded'}`}
                     key={location.pathname}
+                    className={`main-container ${sidebarOpen ? '' : 'expanded'}`}
                 >
                     {children}
                 </div>
